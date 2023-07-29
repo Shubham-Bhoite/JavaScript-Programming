0ec39572ae42f5a88cf5c7201540fa2a4f1c30cb
SHUBHAM BHOITE <shubhambhoite21052002@gmail.com>
2023-07-29 17:28:22 +0530

    add

---
+++ Lessons/Variables.js
@@ -0,0 +1,11 @@
+// Variables: Variables are used to store data.
+
+
+console.log("Hello! Welcome my JavaScript Programming")
+var a = 22 // a contains 22
+console.log(a)
+a = "Shubham"
+console.log(a)
+// let 4shubham = 4 // Not allowed this will throw an error
+// let var = 7
+
--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 
 ## Lessons:
 -  [Introduction to JS](https://github.com/Shubham-Bhoite/JavaScript-Programming/blob/main/Lessons/introduction.md)
+
+- [Variables]()
