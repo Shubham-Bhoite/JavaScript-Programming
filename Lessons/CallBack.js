55b5faeae3d251dbcd931fb3b4e105e5673434c3
SHUBHAM BHOITE <shubhambhoite21052002@gmail.com>
2023-08-09 22:05:30 +0530

    night

---
+++ Lessons/CallBack.js
@@ -0,0 +1,12 @@
+// Syncronous programming ==>
+ let a = prompt("What is your name?");
+ let b = prompt("What is your age?");
+ let c = prompt("What is your favorite color?");
+ console.log(a + " is " + b + " years old and has " + c + " favorite color.");
+
+// Asynchronous programming ==>
+ console.log("Start")
+ setTimeout(function() {
+   console.log("Hey, bro how are you!");
+ }, 3000)
+ console.log("End")
